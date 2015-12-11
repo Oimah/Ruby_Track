b1145a70d37e3d64cd9af078eb1c735414c6ac2a
tonyobiora <tonyobiora26@gmail.com>
2015-12-11 08:44:12 +0000

    updated exercise 2

--- exercise_2/lib/replace_vowel.rb
+++ exercise_2/lib/replace_vowel.rb
@@ -8,7 +8,8 @@ class ReplaceVowel
   def find_vowels_and_replace(input_string)
     vowels = ["a", "e", "i", "o", "u"]
     vowels.each do |v|
-      input_string = input_string. gsub(/#{v}/i , '*')
+
+      input_string = input_string. gsub(/#{v}/i , '*') if ( input_string =~ /#{v}/ )
     end
     print "Vowels replaced in #{input_string}" 
     puts ""
