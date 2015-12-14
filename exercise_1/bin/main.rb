dbb34fbe9397ce671a3d60ee3eca130ee6881f73
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 13:57:34 +0000

    removed class instance of main

--- exercise_1/bin/main.rb
+++ exercise_1/bin/main.rb
@@ -1,12 +1,5 @@
 require_relative "../lib/count_character.rb"
 
-class Main
-
-  def get_input_string
-    puts "enter new string"
-   gets.chomp.to_s.count_alphabet_occurencies
-  end
-
-
-end
-main = Main.new.get_input_string
+ puts "enter new string"
+ gets.chomp.to_s.count_alphabet_occurencies
+  
