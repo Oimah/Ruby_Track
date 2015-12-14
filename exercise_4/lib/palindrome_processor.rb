27250c6877d65a403e9ebbee7ac728f6cf020e0f
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 13:14:41 +0000

    added some white spaces on the code for readability

--- exercise_4/lib/palindrome_processor.rb
+++ exercise_4/lib/palindrome_processor.rb
@@ -3,7 +3,7 @@ class PalindromeProcessor
   def check_if_word_is_palindrome(word)
   	word_palindrome = word.to_s.delete(",").delete(" ").strip.downcase
   	reversed_word = word_palindrome.reverse
-    
+
   	if(word_palindrome == reversed_word)
   	  puts "'#{word}' is a palindrome word"
   	else
@@ -14,8 +14,8 @@ class PalindromeProcessor
 
   	key = gets.chomp.strip
   	if (key.downcase != "q")
-     puts "Enter another string"
-     check_if_word_is_palindrome(gets.to_s)
+      puts "Enter another string"
+      check_if_word_is_palindrome(gets.to_s)
   	end
   end
 end
