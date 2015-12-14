d40d4b0cfc8f6a01d68afa7e2c59280b19906a80
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 09:28:51 +0000

    removed empty spaces

--- exercise_2/lib/replace_vowel.rb
+++ exercise_2/lib/replace_vowel.rb
@@ -4,7 +4,6 @@ class ReplaceVowel
   def find_vowels_and_replace(input_string)
     vowels = ["a", "e", "i", "o", "u"]
     vowels.each do |v|
-
       input_string = input_string. gsub(/#{v}/i , '*') if ( input_string =~ /#{v}/ )
     end
     print "Vowels replaced in #{input_string}" 
