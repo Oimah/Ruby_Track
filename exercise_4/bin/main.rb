530a282897e77ee6066d5b4db117a09b0542f9c4
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 14:04:06 +0000

    removed class instance of main

--- exercise_4/bin/main.rb
+++ exercise_4/bin/main.rb
@@ -1,15 +1,6 @@
 require_relative "../lib/palindrome_processor.rb"
 
-class Main
-  def initialize
-    @palindrome_instance = PalindromeProcessor.new
-  end
-
-  def check_if_word_is_palindrome
-    puts "Enter a string"
-    @palindrome_instance.check_if_word_is_palindrome(gets.chomp.to_s)
-  end
-end
-
-main =Main.new
-main.check_if_word_is_palindrome
+  @palindrome_instance = PalindromeProcessor.new
+  puts "Enter a string"
+  @palindrome_instance.check_if_word_is_palindrome(gets.chomp.to_s)
+  
