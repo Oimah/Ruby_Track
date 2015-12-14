143c542b0ab3ac2b75a41a977f4b44047a270dc4
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 14:35:48 +0000

    removed class instance of main

--- exercise_8/bin/main.rb
+++ exercise_8/bin/main.rb
@@ -1,24 +1,8 @@
 require_relative "../lib/array.rb"
 
-class Main
-def initialize
-  array_from_input = get_input
-  power_input = get_power
- array_powered = array_from_input.power!(power_input)
- puts array_powered
-end
-
-def get_input
- puts "Enter number" 
- string_input = gets.chomp.delete(",").delete(" ")
- string_input = string_input.split("")
-end
-
-def get_power
-puts "Enter power"
-gets.chomp.delete(",").delete(" ").to_i
-end
-
-end
-
- Main.new
+  puts "Enter number" 
+  array_from_input = gets.chomp.delete(",").delete(" ").split("")
+  puts "Enter power"
+  power_input = gets.chomp.delete(",").delete(" ").to_i
+  print array_from_input.power!(power_input)
+  puts 
