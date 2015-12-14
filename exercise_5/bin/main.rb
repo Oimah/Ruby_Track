0e9c2a89d28d61f8d32fe83590d400a15401030e
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 14:20:55 +0000

    removed class instance of main

--- exercise_5/bin/main.rb
+++ exercise_5/bin/main.rb
@@ -1,39 +1,4 @@
 require_relative "../lib/customer.rb"
 
-class Main
-  def  initialize
-  	puts "Customer name"
-  	name = gets.to_s.chomp
-  	customer = CustomerAccountDetail.new(name)
-    action_to_perform(customer)
-  end
-  			
-  def deposit(customer)
-    puts "Deposit Amount"		
-    puts customer.deposit(gets.to_i) 
-    continue_or_not_continue(customer)
-  end
-  def withdraw(customer)	
-    puts "Withdrawal Amount"
-    puts  customer.withdraw(gets.to_i)
-    continue_or_not_continue(customer)
-  end
-
-  def continue_or_not_continue(object_instance)
-    puts "Do you want to continue Y/N"
-    continue_or_not = gets.to_s.chomp
-    continue_or_not.downcase == "y" ? action_to_perform(object_instance): return
-  end
-
-  def action_to_perform(object_instance)
-  	puts "Enter D/d for deposit or W/w to withdraw"
-  	key = gets.to_s.chomp
-  	if(key.downcase == "d" ) 
-  	  deposit(object_instance)
-      else 
-  	  withdraw(object_instance)	
-  	end
-  end
-end
-
-Main.new
+  puts "Customer name"
+  customer = CustomerAccountDetail.new(gets.to_s.chomp)
--- exercise_5/lib/customer.rb
+++ exercise_5/lib/customer.rb
@@ -6,19 +6,42 @@ class CustomerAccountDetail
     number_initializer = number_initializer.to_i + 1
     @account_no = "0000000#{number_initializer}"
     @balance = 1000
+    action_to_perform(self)
   end
 
-  def deposit(amount)
-    @balance += amount
+  def deposit
+    puts "Deposit Amount"    
+    @balance += gets.to_i
+    puts "balance : #{@balance}"
+    continue_or_not_continue(self)
   end
 
-  def withdraw(amount)
+  def withdraw
+    puts "Withdrawal Amount"
+    amount = gets.to_i
     if(@balance >= amount)
-       @balance -= amount 
-       "Transaction successful"
+      @balance -= amount 
+      puts "Transaction successful"
+      puts "balance : #{@balance}"
     else
-      "Insuficient fund"
+      puts "Insuficient fund"
     end
- 
+    continue_or_not_continue(self)
+  end
+
+  def continue_or_not_continue(object_instance)
+    puts "Do you want to continue Y/N"
+    continue_or_not = gets.to_s.chomp
+    continue_or_not.downcase == "y" ? action_to_perform(object_instance): return
+  end
+
+  def action_to_perform(object_instance)
+    puts "Enter D/d for deposit or W/w to withdraw"
+    key = gets.to_s.chomp
+    if(key.downcase == "d" ) 
+      deposit
+      else 
+      withdraw
+    end
   end
 end
