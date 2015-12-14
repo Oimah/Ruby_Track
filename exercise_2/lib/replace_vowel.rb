4f72c0ea9a312461dbd09049a7e0858579dd621d
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 09:26:37 +0000

    removed redundant code

--- exercise_2/lib/replace_vowel.rb
+++ exercise_2/lib/replace_vowel.rb
@@ -1,10 +1,6 @@
 class ReplaceVowel
  
-  def get_input_string
-	  puts "enter new string"
-	  @input_string  = gets.to_s.chomp.downcase
-  end
-
+ 
   def find_vowels_and_replace(input_string)
     vowels = ["a", "e", "i", "o", "u"]
     vowels.each do |v|
