e2cc9aa204c653dab13a06abf4bc9d55a638673a
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 14:01:23 +0000

    removed class instance of main

--- exercise_3/bin/main.rb
+++ exercise_3/bin/main.rb
@@ -1,13 +1,7 @@
 require_relative "../lib/fibonacci_processor.rb"
 
-class Main
-  def Get_number
-    puts "enter a number"
-    gets.to_i.generate_fibonacci_series do |n|
-    print n
-    print ", "
-    end
-  end
+puts "enter a number"
+gets.to_i.generate_fibonacci_series do |n|
+  print n
+  print ", "
 end
-
-Main.new.Get_number
