1584661d97e676d4ddf2d4a09a3cb08e4e8b06e3
tonyobiora <tonyobiora26@gmail.com>
2015-12-14 12:41:07 +0000

    optimized code by extending the string class

--- exercise_1/bin/main.rb
+++ exercise_1/bin/main.rb
@@ -1,19 +1,12 @@
 require_relative "../lib/count_character.rb"
 
 class Main
-def initialize
-  @input_instance_varialble = CountCharacter.new
-end
+
   def get_input_string
     puts "enter new string"
-   @input_instance_varialble.get_input_string(gets.chomp.to_s)
+   gets.chomp.to_s.count_alphabet_occurencies
   end
 
-  def count_alphabet_occurencies
-    @input_instance_varialble.count_alphabet_occurencies
-  end
 
 end
-main = Main.new
-main.get_input_string
-main.count_alphabet_occurencies
+main = Main.new.get_input_string
--- exercise_1/lib/count_character.rb
+++ exercise_1/lib/count_character.rb
@@ -1,18 +1,9 @@
-class CountCharacter
-  attr_accessor :input_value, :alpabets
-	
-	def initialize
-	  @alpabets = ('a'..'z').to_a
-	end
-
-	def get_input_string(string)
-  @input_value  = string
-	end
-
+class String
 	def count_alphabet_occurencies
+		 @alpabets = ('a'..'z').to_a
 	  alphabet_count_hash = Hash.new
 	  @alpabets.each do |character|
-	  occurence_count = @input_value.count(character)
+	  occurence_count = self.count(character)
 		if(occurence_count > 0)
 		  alphabet_count_hash[character] = occurence_count
 		end
