85b3a1ffb10fce2d785c91b52dc7c9e7db04311e
Oimah <obiora@dealdey.com>
2015-12-16 14:22:02 +0000

    optimized code further and removed verbosity

--- exercise_16/lib/high_light_matching_string.rb
+++ exercise_16/lib/high_light_matching_string.rb
@@ -2,6 +2,6 @@
 class String 
 
   def match_string(string_value)
-   puts "Matched string\n #{string_value.gsub(/(#{self})/i , '(\1)')}" 
+   puts "Matched string\n #{string_value.gsub(/(#{self})/i , '(\1)')} ? Total Occurences found: #{string_value.scan(/(?=#{self})/).count}" 
   end
 end
