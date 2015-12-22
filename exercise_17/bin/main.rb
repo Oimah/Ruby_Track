985580a451fdde509774f16ee021ac99d1e25b94
Oimah <obiora@dealdey.com>
2015-12-22 10:08:25 +0000

    correction

--- exercise_17/bin/main.rb
+++ exercise_17/bin/main.rb
@@ -1,8 +1,4 @@
 require_relative "../lib/time_adder.rb"
-
-  return_value = TimeAdder.new.Sum_time("23:23:07", "23:59:59")
-    for time in return_value
-      print time
-    end
+  TimeAdder.new.Sum_time("23:23:07", "23:59:59")
   puts ""
  
--- exercise_17/lib/time_adder.rb
+++ exercise_17/lib/time_adder.rb
@@ -1,42 +1,18 @@
 class TimeAdder 
-
-def Sum_time(first_time, second_time)
-  regex =/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/
-  time_sum_array = Array.new  
-  if(regex.match(first_time) && regex.match(second_time))
-    first_time_array = first_time.split(":")
-    second_time_array = second_time.split(":")
-    size = first_time_array.size
-    temp_value = 0
-    min = 0
-    day = 0
-    (size-1).downto(0).each do |index|
-      place_holder = first_time_array[index].to_i + second_time_array[index].to_i 
-      if(index.to_i == 0)
-        place_holder += min
-        temp_value = (place_holder.to_i / 24).to_f
-        mod  = place_holder.to_i % 24
-        get_day =  temp_value.to_s.split(".")
-        day = get_day[0] if( mod >= 0 && place_holder.to_i >= 24)
-        place_holder = "#{day.to_i > 0 ? day.to_i : ""} #{day.to_i > 0 ? "day" : ""}#{day.to_i > 1 ? "s" : ""}  #{mod}"  if(day.to_i > 0)
-        time_sum_array[index] = place_holder
-      else
-        place_holder += min if(min > 0)
-        min = 0 if(index == 1)
-        temp_value = (place_holder.to_i / 60).to_f
-        mod  = place_holder.to_i % 60
-        get_time =  temp_value.to_s.split(".")
-        time = get_time[0] if( mod >= 0 && place_holder.to_i >= 60)
-        place_holder = ":#{mod > 0 ? mod : ""}"
-        min = time.to_i
-        time_sum_array[index] = place_holder
-      end
-
+  def Sum_time(first_time, second_time)
+    regex =/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/
+    time_sum_array = Array.new  
+    if(regex.match(first_time) && regex.match(second_time))
+      time = first_time.split(":")
+      addtime = second_time.split(":")
+      min_in_sec=(time[2].to_i+addtime[2].to_i)/60
+      cur_sec=(time[2].to_i+addtime[2].to_i)%60
+      hour_in_min=(time[1].to_i+addtime[1].to_i+min_in_sec)/60
+      cur_min=(time[1].to_i+addtime[1].to_i+min_in_sec)%60
+      cur_hour=(time[0].to_i+addtime[0].to_i+hour_in_min)%24
+      day_in_hour=((time[0].to_i+addtime[0].to_i+hour_in_min)/24).to_s.split(".")
+      cur_hour=24 if cur_hour==0
     end
-    else
-      place_holder = "Invalid time format"
-      time_sum_array.push(place_holder)
-     end
-      time_sum_array
-end
+    print "#{day_in_hour[0].to_i  > 0 ? day_in_hour[0].to_i  : ""} #{day_in_hour[0].to_i > 0 ? "day ,": ""} #{cur_hour}:#{cur_min}: #{cur_sec}"
+  end
 end
