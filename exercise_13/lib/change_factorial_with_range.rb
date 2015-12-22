ac1d9849a3f93f44e1b87e4e8c0b8713a39e9898
Oimah <obiora@dealdey.com>
2015-12-22 08:58:47 +0000

    correction

--- exercise_13/lib/change_factorial_with_range.rb
+++ exercise_13/lib/change_factorial_with_range.rb
@@ -1,11 +1,10 @@
 class Fixnum 
-
-def range_factorial_rewrite
-  hash = Hash.new
-  (2..self).collect{|i| 
-    hash["result"] =(hash["result"].to_i == 0 ? 1 : hash["result"].to_i ) * i 
-    hash["factorial"] =hash["factorial"].to_s.insert(0, "#{i} #{i - 1 == 1 ? "* 1 " : "*"} ")  
-  }
-  hash
-end
+  def range_factorial_rewrite
+    hash = Hash.new
+    (2..self).collect{|i| 
+      hash["result"] =(hash["result"].to_i == 0 ? 1 : hash["result"].to_i ) * i 
+      hash["factorial"] =hash["factorial"].to_s.insert(0, "#{i} #{i - 1 == 1 ? "* 1 " : "*"} ")  
+    }
+    hash
+  end
 end
