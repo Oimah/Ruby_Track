8a2d69c92bd27e51a69dd28c0f1ffd781c6d27c9
Oimah <obiora@dealdey.com>
2015-12-22 08:39:50 +0000

    correction

--- exercise_5/bin/main.rb
+++ exercise_5/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative "../lib/customer.rb"
 
   puts "Customer name"
-  customer = CustomerAccountDetail.new(gets.to_s.chomp)
+  customer = CustomerAccountDetail.new(gets.chomp)
--- exercise_5/lib/customer.rb
+++ exercise_5/lib/customer.rb
@@ -31,14 +31,13 @@ class CustomerAccountDetail
 
   def continue_or_not_continue(object_instance)
     puts "Do you want to continue Y/N"
-    continue_or_not = gets.to_s.chomp
+    continue_or_not = gets.chomp
     continue_or_not.downcase == "y" ? action_to_perform(object_instance): return
   end
 
   def action_to_perform(object_instance)
     puts "Enter D/d for deposit or W/w to withdraw"
-    key = gets.to_s.chomp
-    if(key.downcase == "d" ) 
+    if(gets.chomp.downcase == "d" ) 
       deposit
       else 
       withdraw
