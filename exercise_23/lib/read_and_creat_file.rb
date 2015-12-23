4dc09ec4cafba228a72db66dd97d7d80a84e6f78
Oimah <obiora@dealdey.com>
2015-12-23 14:45:41 +0000

    correction

--- exercise_23/lib/read_and_creat_file.rb
+++ exercise_23/lib/read_and_creat_file.rb
@@ -1,16 +1,13 @@
 class ReadAndCreateFile 
   def read_from_and_write_to
     file_param = File.expand_path("../read_and_create_file.csv", __FILE__)
-    #puts file_param
     begin
     raise InvalidFileContentError  unless File.file?(file_param)
-    record_array = File.read(file_param).split(',')
-    file = File.new("../lib/employee_data.csv", "w")
-    (2...record_array.size).each do |index|
-      file.puts "#{record_array[index]}(#{record_array[1]}:#{record_array[index + 1]})" if index.even?
-      end
-    file.close
-     "File writen successfully"
+      record_array = File.read(file_param).split(',')
+      file = File.new("../lib/employee_data.csv", "w")
+      (2...record_array.size).each{|index| file.puts "#{record_array[index]}(#{record_array[1]}:#{record_array[index + 1]})" if index.even?}
+      file.close
+      "File writen successfully"
     rescue InvalidFileContentError
       "No such file"
     end
