102d7b322bcc9dd9b013860e7bc064ca118e684a
Oimah <obiora@dealdey.com>
2015-12-23 14:48:14 +0000

    correction

--- exercise_20/lib/interest.rb
+++ exercise_20/lib/interest.rb
@@ -5,19 +5,19 @@ class Interest
     @rate = 0.1
   end
 
-  def calculate_compound_interest(hash)
+  def compound_interest(hash)
     amount = (hash[:principal].to_i * ((1 + (@rate/hash[:compounded_times].to_i)) ** (hash[:compounded_times].to_i * hash[:duration].to_i)) )
   end
 
-  def calculate_simple_interest(hash)
-    simple_interest = hash[:principal].to_i * @rate * hash[:duration].to_i
+  def simple_interest(hash)
+    _simple_interest = hash[:principal].to_i * @rate * hash[:duration].to_i
   end
 
   def get_amount_for_simple_interest(hash)
-    amount = hash[:principal].to_i + calculate_simple_interest(hash)
+    amount = hash[:principal].to_i + simple_interest(hash)
   end
   def difference_in_interests(hash)
-    diff = calculate_compound_interest(hash) - get_amount_for_simple_interest(hash)
+    diff = compound_interest(hash) - get_amount_for_simple_interest(hash)
   end
 
 end
